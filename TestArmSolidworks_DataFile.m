decf9126b217eaeffb4dc8e33bc1bbf1d336a0f7
AnupaAlwis <isurajithalwis@gmail.com>
2024-09-13 01:55:54 +0530

    file update

---
+++ TestArmSolidworks_DataFile.m
@@ -0,0 +1,195 @@
+% Simscape(TM) Multibody(TM) version: 7.7
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(11).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(11).angle = 0.0;
+smiData.RigidTransform(11).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(11).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0 0 0];  % in
+smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[Robot arm stage-1:-:Robot arm tank stage-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [1.9721522630525295e-31 8.8817841970012523e-16 0];  % in
+smiData.RigidTransform(2).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(2).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(2).ID = "F[Robot arm stage-1:-:Robot arm tank stage-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [4.645669291338586 -0.29585388555036413 0.59024782191635339];  % in
+smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(3).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[Robot arm part 2-1:-:Gripper Hand right-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [-1.1934897514720433e-14 0.43307086614173862 -1.04638520070921e-14];  % in
+smiData.RigidTransform(4).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(4).axis = [0.57735026918962584 -0.57735026918962595 0.57735026918962562];
+smiData.RigidTransform(4).ID = "F[Robot arm part 2-1:-:Gripper Hand right-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [6.0576622159156495 0.18297419332722267 0.98425196850393704];  % in
+smiData.RigidTransform(5).angle = 0;  % rad
+smiData.RigidTransform(5).axis = [0 0 0];
+smiData.RigidTransform(5).ID = "B[Robot arm part 1-1:-:Robot arm part 2-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [-2.7613965543528338 0.3625120499211969 -0.19685039370079427];  % in
+smiData.RigidTransform(6).angle = 2.0762052638239656e-16;  % rad
+smiData.RigidTransform(6).axis = [0.56022752206430237 0.82833877340233908 4.8174001641231501e-17];
+smiData.RigidTransform(6).ID = "F[Robot arm part 1-1:-:Robot arm part 2-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-1.8163535321158502 0.18297419332722051 0.98425196850393704];  % in
+smiData.RigidTransform(7).angle = 0;  % rad
+smiData.RigidTransform(7).axis = [0 0 0];
+smiData.RigidTransform(7).ID = "B[Robot arm part 1-1:-:Robot arm stage-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-4.3801767768414379e-17 2.1653543307086633 -0.59055118110235849];  % in
+smiData.RigidTransform(8).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(8).axis = [1 -8.9152469948768923e-33 -9.1800934697120972e-17];
+smiData.RigidTransform(8).ID = "F[Robot arm part 1-1:-:Robot arm stage-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [4.645669291338586 -0.29585388555036357 0.19685039370078716];  % in
+smiData.RigidTransform(9).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(9).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(9).ID = "B[Robot arm part 2-1:-:Gripper right hand-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [-2.2204460492503131e-16 -0.039370078740151165 -1.4675760606763788e-15];  % in
+smiData.RigidTransform(10).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(10).axis = [-0.57735026918962562 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(10).ID = "F[Robot arm part 2-1:-:Gripper right hand-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-3.8093188261441875 0.59976384735924848 -1.9025352666557853];  % in
+smiData.RigidTransform(11).angle = 1.5707963267948966;  % rad
+smiData.RigidTransform(11).axis = [0 0 -1];
+smiData.RigidTransform(11).ID = "RootGround[Robot arm tank stage-2]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(6).mass = 0.0;
+smiData.Solid(6).CoM = [0.0 0.0 0.0];
+smiData.Solid(6).MoI = [0.0 0.0 0.0];
+smiData.Solid(6).PoI = [0.0 0.0 0.0];
+smiData.Solid(6).color = [0.0 0.0 0.0];
+smiData.Solid(6).opacity = 0.0;
+smiData.Solid(6).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.090995574287564246;  % kg
+smiData.Solid(1).CoM = [52.138386520590181 4.6475445105114241 10.215779220458414];  % mm
+smiData.Solid(1).MoI = [6.5595536409031476 385.57747368436338 384.90103209117171];  % kg*mm^2
+smiData.Solid(1).PoI = [0 -1.9973899291831039 0];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "Robot arm part 1*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.0894953729652819;  % kg
+smiData.Solid(2).CoM = [29.632394770957838 8.9333346865426009 9.7806012613027775];  % mm
+smiData.Solid(2).MoI = [6.3993997647242145 365.28586585205892 365.36221563664151];  % kg*mm^2
+smiData.Solid(2).PoI = [0.0053856495586941571 -1.9589968893620069 2.049970634326328];  % kg*mm^2
+smiData.Solid(2).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "Robot arm part 2*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.071837608730292102;  % kg
+smiData.Solid(3).CoM = [4.1222608231279394e-10 16.093724034618457 1.8152541033327726e-05];  % mm
+smiData.Solid(3).MoI = [68.461059984171598 67.544634459021395 52.070280086695313];  % kg*mm^2
+smiData.Solid(3).PoI = [0.000213151850512924 1.1102033390310968e-05 0];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "Robot arm stage*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.0066638097751150691;  % kg
+smiData.Solid(4).CoM = [39.451743914591141 4.9766636741442687 5.3772460430572853];  % mm
+smiData.Solid(4).MoI = [2.1158239639640422 4.9116219196781374 2.9080982646470779];  % kg*mm^2
+smiData.Solid(4).PoI = [-0.0008362092749380557 -1.5806784369583089 -0.0061350947882427391];  % kg*mm^2
+smiData.Solid(4).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "Gripper right hand*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.14254976665663685;  % kg
+smiData.Solid(5).CoM = [7.5 0 0];  % mm
+smiData.Solid(5).MoI = [215.60652206816326 110.47606915889355 110.47606915889358];  % kg*mm^2
+smiData.Solid(5).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(5).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = "Robot arm tank stage*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(6).mass = 0.0065596454606954829;  % kg
+smiData.Solid(6).CoM = [38.008666057827128 5.0237068965517251 9.4345366224947647];  % mm
+smiData.Solid(6).MoI = [2.3493529758842007 4.6848499013611891 2.4460610472489419];  % kg*mm^2
+smiData.Solid(6).PoI = [0.0014671538116910227 -1.5528147209063581 0.0059106834299708757];  % kg*mm^2
+smiData.Solid(6).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(6).opacity = 1;
+smiData.Solid(6).ID = "Gripper Hand right*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(5).Rz.Pos = 0.0;
+smiData.RevoluteJoint(5).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = -90.196815551062969;  % deg
+smiData.RevoluteJoint(1).ID = "[Robot arm stage-1:-:Robot arm tank stage-2]";
+
+smiData.RevoluteJoint(2).Rz.Pos = -133.41923099267515;  % deg
+smiData.RevoluteJoint(2).ID = "[Robot arm part 2-1:-:Gripper Hand right-1]";
+
+smiData.RevoluteJoint(3).Rz.Pos = -3.3705342506977467;  % deg
+smiData.RevoluteJoint(3).ID = "[Robot arm part 1-1:-:Robot arm part 2-1]";
+
+smiData.RevoluteJoint(4).Rz.Pos = 147.47521873441437;  % deg
+smiData.RevoluteJoint(4).ID = "[Robot arm part 1-1:-:Robot arm stage-1]";
+
+smiData.RevoluteJoint(5).Rz.Pos = 131.44675635078403;  % deg
+smiData.RevoluteJoint(5).ID = "[Robot arm part 2-1:-:Gripper right hand-1]";
+
